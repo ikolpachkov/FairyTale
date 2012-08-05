627e939a2807cf7dbdbac80d29a440202ec9ef87
xorosho0 <sevjewelry@gmail.com>
2012-08-05 14:18:20 +0300

    initial commit

---
+++ FairyTale_First/FairyTale_First/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  FairyTale_First
+//
+//  Created by Igor Kolpachkov on 05.08.12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ FairyTale_First/FairyTale_First/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  FairyTale_First
+//
+//  Created by Igor Kolpachkov on 05.08.12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
